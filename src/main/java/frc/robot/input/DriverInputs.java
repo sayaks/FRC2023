3c9da7a7abb804cf234f12ef2f5252e5c532622a
JamesBeeProg <jamesbeeprog@gmail.com>
2023-01-25 18:26:38 -0500

    Refactor DriverInputs

--- src/main/java/frc/robot/input/DriverInputs.java
+++ src/main/java/frc/robot/input/DriverInputs.java
@@ -8,44 +8,53 @@ import org.assabet.aztechs157.input.layouts.SelectableLayout;
 import org.assabet.aztechs157.input.models.LogitechExtreme3D;
 import org.assabet.aztechs157.input.models.LogitechGamepadF310;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import frc.robot.Constants.DriveConstants;
 
-public class DriverInputs {
+public class DriverInputs extends SelectableLayout {
     public static final Axis.Key driveSpeedX = new Axis.Key().label("Drive Speed X");
     public static final Axis.Key driveSpeedY = new Axis.Key().label("Drive Speed Y");
     public static final Axis.Key driveRotation = new Axis.Key().label("Drive Rotation");
 
-    public static Layout getDriverInputs() {
+    private static final NetworkTableEntry entry = NetworkTableInstance.getDefault().getEntry("157/Drive/StickEnabled");
 
-        final var logitechLayout = new MapLayout();
-        final var logitech = new LogitechGamepadF310(0);
-        logitechLayout.assign(driveSpeedX, manualDeadzone(logitech.leftStickX).scaled(0.5));
-        logitechLayout.assign(driveSpeedY, manualDeadzone(logitech.leftStickY).scaled(0.5));
-        logitechLayout.assign(driveRotation,
-                manualDeadzone(logitech.rightStickX).scaled(DriveConstants.TELEOP_SPIN_SPEED));
-
-        final var stickLayout = new MapLayout();
-        final var flightStick = new LogitechExtreme3D(1);
-
-        final var flightStickSpeedModifier = flightStick.slider.inverted()
-                .convertRange(new Range(-1, 1), new Range(0, 1));
-
-        stickLayout.assign(driveSpeedX, manualDeadzone(flightStick.stickX).scaled(flightStickSpeedModifier::get));
-        stickLayout.assign(driveSpeedY, manualDeadzone(flightStick.stickY).scaled(flightStickSpeedModifier::get));
-        stickLayout.assign(driveRotation,
-                manualDeadzone(flightStick.stickRotate).scaled(DriveConstants.TELEOP_SPIN_SPEED));
-
-        final var entry = NetworkTableInstance.getDefault().getEntry("157/Drive/StickEnabled");
-        entry.setDefaultBoolean(false);
-
-        return new SelectableLayout(
-                () -> entry.getBoolean(false)
-                        ? stickLayout
-                        : logitechLayout);
+    public DriverInputs() {
+        super(() -> entry.getBoolean(false)
+                ? flightLayout()
+                : logitechLayout());
     }
 
-    public static Axis manualDeadzone(final Axis input) {
+    private static final double kMaxRotationDegreesPerSecond = 50;
+
+    private static Layout logitechLayout() {
+        final var layout = new MapLayout();
+        final var input = new LogitechGamepadF310(0);
+
+        final var speedModifier = 0.5;
+
+        layout.assign(driveSpeedX, manualDeadzone(input.leftStickX).scaled(speedModifier));
+        layout.assign(driveSpeedY, manualDeadzone(input.leftStickY).scaled(speedModifier));
+        layout.assign(driveRotation, manualDeadzone(input.rightStickX).scaled(kMaxRotationDegreesPerSecond));
+
+        return layout;
+    }
+
+    private static Layout flightLayout() {
+        final var layout = new MapLayout();
+        final var input = new LogitechExtreme3D(1);
+
+        final var speedModifier = input.slider.inverted()
+                .convertRange(new Range(-1, 1), new Range(0, 1));
+
+        layout.assign(driveSpeedX, manualDeadzone(input.stickX).scaled(speedModifier::get));
+        layout.assign(driveSpeedY, manualDeadzone(input.stickY).scaled(speedModifier::get));
+        layout.assign(driveRotation, manualDeadzone(input.stickRotate).scaled(kMaxRotationDegreesPerSecond));
+
+        return layout;
+    }
+
+    private static Axis manualDeadzone(final Axis input) {
         return input.map(value -> {
             if (Math.abs(value) < DriveConstants.TELEOP_AXIS_THRESHOLD) {
                 return 0;
