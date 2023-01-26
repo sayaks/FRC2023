8c94215f8221d1cc7438df23b958271db302fa6b
JamesBeeProg <jamesbeeprog@gmail.com>
2023-01-25 19:57:33 -0500

    Add more to Expect

--- src/main/java/org/assabet/aztechs157/Expect.java
+++ src/main/java/org/assabet/aztechs157/Expect.java
@@ -16,6 +16,21 @@ public final class Expect {
     }
 
     public static record ExpectNumber(double value) {
+        public ExpectNumber equalTo(final double other) {
+            if (value == other) {
+                return this;
+            } else {
+                throw new ExpectError(value + " was not equal to " + other);
+            }
+        }
+
+        public ExpectNumber notEqualTo(final double other) {
+            if (value != other) {
+                return this;
+            } else {
+                throw new ExpectError(value + " was equal to " + other);
+            }
+        }
 
         public ExpectNumber greaterThan(final double other) {
             if (value > other) {
@@ -46,6 +61,20 @@ public final class Expect {
                 return this;
             } else {
                 throw new ExpectError(value + " was not less or equal to " + other);
+            }
+        }
+    }
+
+    public static ExpectBoolean bool(final boolean value) {
+        return new ExpectBoolean(value);
+    }
+
+    public static record ExpectBoolean(boolean value) {
+        public ExpectBoolean equal(final boolean other) {
+            if (value == other) {
+                return this;
+            } else {
+                throw new ExpectError(value + " was not equal to " + other);
             }
         }
     }
