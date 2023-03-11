6ea0033bfd5a402bbd3ba08ac542c79622efced5
JamesBeeProg <JamesBeeProg@gmail.com>
2023-03-10 20:35:59 -0500

    Added code to use a rev blinkin -Ayden

---
+++ src/main/java/frc/robot/cosmetics/LightsSubsystem.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.cosmetics;
+
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.CosmeticConstants;
+
+public class LightsSubsystem extends SubsystemBase {
+    public final Spark lightController = new Spark(CosmeticConstants.LIGHT_ID);
+    private ShuffleboardTab tab = Shuffleboard.getTab("LED");
+    private GenericEntry lightColor = tab.add("led color", CosmeticConstants.SOLID_YELLOW_VALUE).getEntry();
+
+    /** Creates a new lights. */
+    public LightsSubsystem() {
+    }
+
+    public void setYellow() {
+        lightController.set(CosmeticConstants.SOLID_YELLOW_VALUE);
+    }
+
+    public void setPurple() {
+        lightController.set(CosmeticConstants.SOLID_PURPLE_VALUE);
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+        // lightController.set(lightColor.getDouble(CosmeticConstants.SOLID_YELLOW_VALUE));
+    }
+}
