b5898e2acd51065a3d12535145d4d4c3097652bc
JamesBeeProg <JamesBeeProg@gmail.com>
2023-03-17 20:28:07 -0400

    Added comments for auto testing at WPI and added the auto to the selector - Eric

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -107,6 +107,7 @@ public class RobotContainer {
         chooser.addOption("scoreHighThenEngage", scoreHighThenEngage());
         chooser.addOption("leaveCommunityThenEngage", leaveCommunityThenEngage());
         chooser.addOption("everythingIsBrokenDoNothing", new InstantCommand(() -> System.out.println(":(")));
+        chooser.addOption("twoPiecethenEngage", TwoPieceThenEngage());
     }
 
     /**
@@ -205,22 +206,23 @@ public class RobotContainer {
     }
 
     public Command TwoPieceThenEngage() {
+        // for wpi, might need to change desired angle and definitely distance gone
+        // Figure out what side this works on, then mirror it for the opposite color
         return new SequentialCommandGroup(intakeSubsystem.intake(-1).withTimeout(0.75),
                 new ParallelCommandGroup(driveSubsystem.driveWithRotation(0, 1, 0),
                         group.lowPosCommand(1),
-                        intakeSubsystem.intake(1)
-                ).withTimeout(3),
+                        intakeSubsystem.intake(1)).withTimeout(3),
                 new ParallelCommandGroup(driveSubsystem.driveWithRotation(180, -1, 0),
                         group.startingPosCommand(1),
-                        intakeSubsystem.intake(0.1)
-                ).withTimeout(3),
+                        intakeSubsystem.intake(0.1)).withTimeout(3),
                 group.highPosCommand(1).withTimeout(1.3),
                 intakeSubsystem.ejectCargo().withTimeout(0.5),
                 new ParallelCommandGroup(group.startingPosCommand(1),
-                    new WaitCommand(0.5).andThen(driveSubsystem.driveWithRotation(0, 0.5, .5))
-                ).withTimeout(2.25),
-                new AutoBalance(driveSubsystem)
-        );
+                        new WaitCommand(0.5).andThen(driveSubsystem.driveWithRotation(0, 0.5, .5)))
+                        .withTimeout(0.5 + 1.75), // first value is the wait, second value is the drive time, and maybe
+                                                  // increase Y to adjust for charge station (if hit charge station
+                                                  // side, increase Y) maybe add a forward to get further up platform
+                new AutoBalance(driveSubsystem));
     }
 
     /*
